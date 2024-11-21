5fc15e2bbc645c8d714bf38c64c19a267ae6d601
Daniel Akilo <kayodeakilo1@gmail.com>
2024-11-21 19:12:09 +0200

    changing tests

--- app/src/test/java/com/kayodedaniel/gogovmobile/SupabaseConnectionTest.kt
+++ app/src/test/java/com/kayodedaniel/gogovmobile/SupabaseConnectionTest.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
 import org.junit.Test
 
 class SupabaseConnectionTest {
@@ -17,9 +18,14 @@ class SupabaseConnectionTest {
             .url("https://bgckkkxjfnkwgjzlancs.supabase.co/rest/v1")
             .build()
 
-        client.newCall(request).execute().use { response ->
-            assertTrue("Connection to Supabase failed", response.isSuccessful)
-            assertEquals(200, response.code)
+        try {
+            client.newCall(request).execute().use { response ->
+                assertTrue("Connection to Supabase failed", response.isSuccessful)
+                assertEquals(200, response.code)
+            }
+        } catch (e: Exception) {
+            fail("Exception occurred: ${e.message}")
         }
     }
+
 }
