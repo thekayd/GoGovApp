d391f4eacff25cddf459822eaa4b8b5f30bdad12
Daniel Akilo <kayodeakilo1@gmail.com>
2024-11-21 20:03:44 +0200

    Added more security to pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.sonarqube") version "4.0.0.2929"
+    id("org.owasp.dependencycheck") version "8.4.0"
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     kotlin("plugin.serialization") version "1.9.0"
